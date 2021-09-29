7e6f4efc440239fca87405cc9b7c76d8446ec6f9
Jingyi Lin <linjingyikayla@gmail.com>
2021-09-28 23:34:20 -0400

    add function, css

--- connect4.js
+++ connect4.js
@@ -8,8 +8,8 @@
 var WIDTH = 7;
 var HEIGHT = 6;
 
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+let currPlayer = 1; // active player: 1 or 2
+let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
@@ -17,13 +17,17 @@ var board = []; // array of rows, each row is array of cells  (board[y][x])
 
 function makeBoard() {
   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+ for (let i = 0; i < HEIGHT; i++) {
+    board.push(new Array(WIDTH));
+    // board.push(Array.from({ length: WIDTH }));
+ }
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
   // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
-
+  let htmlBoard = document.querySelector('#board');
   // TODO: add comment for this code
   var top = document.createElement("tr");
   top.setAttribute("id", "column-top");
@@ -37,7 +41,7 @@ function makeHtmlBoard() {
   htmlBoard.append(top);
 
   // TODO: add comment for this code
-  for (var y = 0; y < HEIGHT; y++) {
+  for (let y = 0; y < HEIGHT; y++) {
     const row = document.createElement("tr");
     for (var x = 0; x < WIDTH; x++) {
       const cell = document.createElement("td");
@@ -52,29 +56,48 @@ function makeHtmlBoard() {
 
 function findSpotForCol(x) {
   // TODO: write the real version of this, rather than always returning 0
-  return 0;
+//   let block = document.getElementById('${y}-${x}')
+    for (let y = 5; y >= 0; y--){
+        if (board[y][x] === undefined){
+            board[y][x] = 1;
+            return y;
+        }
+    }
+    return null;
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
   // TODO: make a div and insert into correct table cell
+    let eachBlock = document.getElementsByTagName("td")[`${y}-${x}`];
+    let div = document.createElement('div');
+    let p = document.createElement('p');
+    if(currPlayer === 1) {
+        p.classList = "dot1";
+    } else if(currPlayer === 2) {
+        p.classList = "dot2";
+    }
+    div.append(p);
+    eachBlock.append(div);
 }
 
 /** endGame: announce game end */
 
 function endGame(msg) {
   // TODO: pop up alert message
+  alert(msg);
 }
 
 /** handleClick: handle click of column top to play piece */
 
 function handleClick(evt) {
   // get x from ID of clicked cell
-  var x = +evt.target.id;
+  let x = +evt.target.id;
 
   // get next spot in column (if none, ignore click)
-  var y = findSpotForCol(x);
+  let y = findSpotForCol(x);
+//   console.log("x" + x + " y" + y);
   if (y === null) {
     return;
   }
@@ -82,17 +105,55 @@ function handleClick(evt) {
   // place piece in board and add to HTML table
   // TODO: add line to update in-memory board
   placeInTable(y, x);
+  board[y][x] = currPlayer;
 
   // check for win
   if (checkForWin()) {
+    console.log(`Player ${currPlayer} won!`);
     return endGame(`Player ${currPlayer} won!`);
   }
 
   // check for tie
   // TODO: check if all cells in board are filled; if so call, call endGame
+//  if (board[0].every(function(val) {
+//         return val;    
+//  })){
+//     console.log("Game End");
+//     return endGame("Game End");
+//  }
+
+
+//  if(board[0].every((val) => val)){
+//     return endGame("Game End");
+//  }
+
+ let full = true;
+ for (let i = 0; i < WIDTH; i ++) {
+     if (board[0][i] !== 1 && board[0][i] !== 2) {
+        full = false;
+     }
+ }
+ if (full) {
+     return endGame("Game End")
+ }
+
+//  if (board.every(function(row){
+//     return row.every(function(cell){
+//         return cell === true;
+//     })
+//  })){
+//     return endGame("Game End");
+//  }
+
+
+
+
+
+
 
   // switch players
   // TODO: switch currPlayer 1 <-> 2
+  currPlayer = currPlayer === 1 ? 2 : 1;
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
@@ -117,9 +178,13 @@ function checkForWin() {
 
   for (var y = 0; y < HEIGHT; y++) {
     for (var x = 0; x < WIDTH; x++) {
+        // horizontal 4 dots
       var horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+       // vertical 4 dots
       var vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+       // diag right 4 dots
       var diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+       // diag left 4 dots
       var diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
 
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
